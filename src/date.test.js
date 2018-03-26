b11d8650c70df3bb704a0a9b661019d1006cd54b
kenlyau <newallah@gmail.com>
2018-03-26 15:25:53 +0800

    write test file

--- src/date.test.js
+++ src/date.test.js
@@ -5,6 +5,7 @@ describe('test date function', () => {
     expect(date().format('yyyy')).toBe(new Date().getFullYear() + '')
   })
   test('test 1511325820264 === 2017-11-22 12:43', () => {
-    expect(date(1511325820264).format('yyyy-MM-dd hh:mm')).toBe('2017-11-22 12:43')
+    const now = new Date(1511325820264)
+    expect(date(1511325820264).format('yyyy-MM-dd hh:mm')).toBe(`${now.getFullYear()}-${now.getMonth() + 1}-${now.getDate()} ${now.getHours()}:${now.getMinutes()}`)
   })
 })
--- src/group.test.js
+++ src/group.test.js
@@ -1,4 +1,8 @@
 import group from './group'
 
 describe('test group', () => {
+  const originGroup = [1,2,3,4,5,6,7]
+  test('test group length', () => {
+    expect(group(originGroup, 3)).toHaveLength(3)
+  })
 })
